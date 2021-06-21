adf1a2d3489556165abd80da6afab97f4060ecb5
David Rock <davidrocha@outlook.com>
2021-06-21 14:23:03 -0300

    update episodes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,8 +3,28 @@ import { useEffect } from "react";
 import { mergeMap } from "rxjs/operators";
 import Header from "../components/Header";
 import { fromFetch } from "rxjs/fetch";
+import { GetStaticProps } from "next";
+import { api } from "../services/api";
+import { format, parseISO } from "date-fns";
+import { ptBR } from "date-fns/locale";
+import { convertDurationToTimeString } from "../utils/convertDurationToTimeString";
 
-export default function Home(props) {
+type Episode = {
+  id: string;
+  title: string;
+  members: string;
+  publishedAt: Date;
+  duration: number;
+  durationAsString: string;
+  description: string;
+  url: string;
+};
+
+type HomeProps = {
+  episodes: Episode[];
+};
+
+export default function Home(props: HomeProps) {
   // useEffect(() => {
   //   fromFetch("http://localhost:5000/episodes")
   //     .pipe(mergeMap((res) => res.json()))
@@ -20,15 +40,38 @@ export default function Home(props) {
   );
 }
 
-export async function getStaticProps() {
-  const response = await fetch("http://localhost:5000/episodes");
-  const data = await response.json();
+export const getStaticProps: GetStaticProps = async () => {
+  const { data } = await api.get("episodes", {
+    // params: {
+    //   _limit: 12,
+    //   _sort: "published_at",
+    //   _order: "desc",
+    // },
+  });
+
+  const eps = data.map((episode) => {
+    return {
+      id: episode.id,
+      title: episode.title,
+      thumbnail: episode.thumbnail,
+      members: episode.members,
+      publishedAt: format(parseISO(episode.published_at), "d MMM yy", {
+        locale: ptBR,
+      }),
+      duration: Number(episode.file.duration),
+      durationAsString: convertDurationToTimeString(
+        Number(episode.file.duration)
+      ),
+      description: episode.description,
+      url: episode.file.url,
+    };
+  });
 
   return {
     props: {
-      episodes: data,
+      episodes: eps,
     },
 
     revalidate: 60 * 60 * 8,
   };
-}
+};
